222e181b3d5e533f85c0ec19efe91e55130afa47
david robison <mrdavidrobison@gmail.com>
2017-07-26 21:42:35 -0600

    use reqs, specify params and listen

--- app.js
+++ app.js
@@ -3,4 +3,19 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
+app.use(bodyParser.json());
+app.use(cors());
 
+app.use(express.static(__dirname +'/public'));
+
+app.get('/new/:urlToShorten(*)', (req, res, next)=>{
+  var { urlToShorten } = req.params;
+  console.log(urlToShorten);
+});
+
+//Listen to see if working
+// (ES5) function(){}
+// process.env.PORT is for Heroku
+app.listen(process.env.PORT || 3000, ()=>{
+console.log('working');
+});
