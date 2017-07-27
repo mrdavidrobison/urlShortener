18a62573686e1e3b97ce1a942ce74426537a64f8
david robison <mrdavidrobison@gmail.com>
2017-07-26 23:24:10 -0600

    connect to mongodb

--- app.js
+++ app.js
@@ -7,8 +7,14 @@ const shortUrl = require('.models/shorturl');
 app.use(bodyParser.json());
 app.use(cors());
 
+// connect to database
+mongoose.connect(process.env.MONGODB_URI  || 'mongodb://localhost/shortUrls');
+
+
+
 app.use(express.static(__dirname +'/public'));
 
+// create database entry
 app.get('/new/:urlToShorten(*)', (req, res, next)=>{
   var { urlToShorten } = req.params;
   return res.json({urlToShorten});
