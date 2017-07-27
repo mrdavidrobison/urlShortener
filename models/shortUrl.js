887f9856cecc435c2b0455f1087ed444dc27902b
david robison <mrdavidrobison@gmail.com>
2017-07-26 23:23:59 -0600

    create schema

--- models/shortUrl.js
+++ models/shortUrl.js
@@ -1,3 +1,11 @@
 const mongoose = require('mongoose');
 const schema = mongoose.schema;
 
+const urlSchema = new schema({
+  origUrl = string,
+  shorterUrl = string
+}, {timestamps: true});
+
+const modelClass = mongoose.model('shortUrl', urlSchema);
+
+module.exports = modelClass;
