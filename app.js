d84eb2b96723d487db109f934fdc8a43efa3e5e7
david robison <mrdavidrobison@gmail.com>
2017-09-23 09:59:16 -0600

    include regex for url

--- app.js
+++ app.js
@@ -17,6 +17,8 @@ app.use(express.static(__dirname +'/public'));
 // create database entry
 app.get('/new/:urlToShorten(*)', (req, res, next)=>{
   var { urlToShorten } = req.params;
+  // Regex for url
+  var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
   return res.json({urlToShorten});
 });
 
