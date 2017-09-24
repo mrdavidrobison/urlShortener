433f10e55799736768bdab8eeda9c025e10f067e
david robison <mrdavidrobison@gmail.com>
2017-09-24 08:44:17 -0700

    data.save if else stmt

--- app.js
+++ app.js
@@ -20,12 +20,28 @@ app.get('/new/:urlToShorten(*)', (req, res, next)=>{
   // Regex for url
   var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
   var regex = expression;
+
   if(regex.test(urlToShorten) === true) {
-    return 'works';
-  } else {
-    return 'fails';
+    var short = math.floor(Math.random()*100000).toString();
+    var data = new shortUrl(
+      {
+        originalUrl: urlToShorten,
+        shorterUrl: short
+      }
+    )
+
+    data.save(err=> {
+      if(err) {
+        return res.send('Error saving to the db');
+      } else {
+
+      }
+    });
+
+    return res.json({data});
   }
-  return res.json({urlToShorten});
+
+  return res.json({urlToShorten: 'failed'});
 });
 
 app.listen(process.env.PORT || 3000, ()=>{
